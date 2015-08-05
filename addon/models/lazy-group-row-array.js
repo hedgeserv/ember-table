daaa511dcbcfe0730a43d2aba9dcfb288e02d42c
Chaoyang <cyjia@thoughtworks.com>
2015-08-05 17:11:13 +0800

    clean up.

--- addon/models/lazy-group-row-array.js
+++ addon/models/lazy-group-row-array.js
@@ -57,46 +57,22 @@ var LazyGroupRowArray = Ember.ArrayProxy.extend({
     this.setObjects(Ember.A([LoadingPlaceHolder.create()]));
   },
 
-  _content: Ember.computed(function() {
-    return this.get('content');
-  }).property('content'),
-
   sortingColumns: Ember.computed.oneWay('root.sortingColumns'),
-
-  needSort: Ember.computed.and('sortingColumns.isNotEmpty', 'grouping.isLeafParent'),
-
-  sortedContent: Ember.computed(function() {
-    var content = this.get('content');
-    var sortingColumns = this.get('sortingColumns');
-    return sortingColumns.sortContent(content);
-  }).property('sortingColumns._columns', 'content'),
-
-  /*---------------Override ArrayProxy -----------------------*/
-  objectAtContent: function (index) {
-    var object = this.get('_content').objectAt(index);
-    if (object && object.get('isLoading') && !this.get('_hasInProgressLoading')) {
-      //this.triggerLoading(index);
-    }
-    return object;
-  },
 
   /*---------------Private methods -----------------------*/
   triggerLoading: function (index, loadWatcher) {
-    this.set('_hasInProgressLoading', true);
     var chunkIndex = this.chunkIndex(index);
     var group = this.get('grouping.key');
     var self = this;
     this.incrementProperty('status.loadingCount');
     this.loadOneChunk(chunkIndex).then(function (result) {
       self.onOneChunkLoaded(result);
-      self.set('_hasInProgressLoading', false);
       self.notifyPropertyChange('length');
       self.decrementProperty('status.loadingCount');
       if (loadWatcher) {
         loadWatcher.notifyOneChunkLoaded();
       }
     }).catch(function() {
-      self.set('_hasInProgressLoading', false);
       self.onLoadError("Failed to load data.", group, chunkIndex);
       self.decrementProperty('status.loadingCount');
     });
@@ -111,7 +87,7 @@ var LazyGroupRowArray = Ember.ArrayProxy.extend({
   },
 
   onOneChunkLoaded: function (result) {
-    var _content = this.get('_content');
+    var content = this.get('content');
     this.setProperties(result.meta);
     var chunk = result.content;
     if (chunk.get('length') > 0) {
@@ -120,12 +96,12 @@ var LazyGroupRowArray = Ember.ArrayProxy.extend({
       var chunkObjects = chunk.slice(1).map(function (x) {
         return Ember.ObjectProxy.create({"isLoaded": true, "isError": false, "content": self.wrapLoadedContent(x)});
       });
-      _content.pushObjects(chunkObjects);
+      content.pushObjects(chunkObjects);
       if (this.get('length') < this.get('totalCount')) {
-        this.addLoadingPlaceHolder('_content');
+        this.addLoadingPlaceHolder('content');
       }
     } else {
-      _content.removeObject(this.get('lastObject'));
+      content.removeObject(this.get('lastObject'));
     }
   },
 
@@ -135,25 +111,13 @@ var LazyGroupRowArray = Ember.ArrayProxy.extend({
   },
 
   updatePlaceHolderWithContent: function (content) {
-    var _content = this.get('_content');
-    var lastObject = _content.get('lastObject');
+    var lastObject = this.get('content.lastObject');
     lastObject.set('content', content);
   },
-
-  length: Ember.computed.oneWay('_content.length'),
-
-  isCompleted: Ember.computed(function(){
-    var content = this.get('content');
-    return !content.some(function(item){
-      return item.get('isLoading');
-    });
-  }).property('content.@each.isLoading'),
 
   totalCount: null,
 
   chunkSize: null,
-
-  _hasInProgressLoading: false,
 
   loadingCount: Ember.computed.oneWay('status.loadingCount'),
 
